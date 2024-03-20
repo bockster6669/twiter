67da751602079b03ee6d7a6ae0f0a82318c010e4
Versus Development <bockster6669@gmail.com>
2024-03-19 20:37:34 -0800

    update

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: 'https://bockster6669.github.io/twiter/'
+  base: 'https://bockster6669.github.io/'
 })
